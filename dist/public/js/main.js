c520204445d1e0df8363f73dd22e73d790dd6de0
Yosep Ryan Agusta <yosep.ryan.yra@gmail.com>
2019-10-26 20:10:50 +0700

    Replaced color for X and O marks

--- dist/public/js/main.js
+++ dist/public/js/main.js
@@ -1,28 +1,37 @@
-let board = document.querySelector('.board');
-let boxes = Array.from(document.querySelectorAll('.box'));
+let boxes = document.querySelectorAll('.box');
+let boxesArray = Array.from(boxes);
 let alertBox = document.querySelector('.alertBox');
 
 // Model
 let tictactoe = {
   board: ['', '', '', '', '', '', '', '', ''],
   currentPlayer: 1,
-  status: 'playing'
+  status: 'playing',
+  mark: ['X', 'O']
 };
 
 // Check
 // → function to be called when user click on the box.
 function check(e) {
+  if (tictactoe.status !== 'playing') return;
   let box = e.target;
-  let color = tictactoe.currentPlayer === 1 ? 'blue' : 'green';
-  // Change the view.
-  box.classList.add(color);
-  // Update the model
-  updateModel(box);
+  let mark = tictactoe.mark[tictactoe.currentPlayer - 1];
+
+  // If it's already chosen before.
+  if (!box.className.includes('clicked')) {
+    // Update the view.
+    box.classList.add('clicked');
+    box.textContent = mark;
+    // Update the model
+    updateModel(box);
+  } else {
+    return;
+  }
 }
 
 // Update model
 function updateModel(box) {
-  let position = boxes.indexOf(box);
+  let position = boxesArray.indexOf(box);
   tictactoe.board[position] = tictactoe.currentPlayer;
 
   // Check win
@@ -60,7 +69,9 @@ function checkWin() {
 
 function init() {
   alertBox.textContent = `Player ${tictactoe.currentPlayer}'s turn`;
-  board.addEventListener('click', check);
+  boxes.forEach(box => {
+    box.addEventListener('click', check);
+  });
 }
 
 init();
